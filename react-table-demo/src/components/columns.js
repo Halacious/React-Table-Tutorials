0f2d52a3591785be95ad858658265b55dcba670c
Hal <hal.burch@dotfoods.com>
2022-03-15 13:50:53 -0500

    table for orders

--- react-table-demo/src/components/columns.js
+++ react-table-demo/src/components/columns.js
@@ -9,88 +9,78 @@ export const COLUMNS = [
     sticky: 'left'
   },
   {
-    Header: 'First Name',
-    Footer: 'First Name',
-    accessor: 'first_name',
+    Header: 'CO #',
+    Footer: 'CO #',
+    accessor: 'co_number',
     sticky: 'left'
   },
   {
-    Header: 'Last Name',
-    Footer: 'Last Name',
-    accessor: 'last_name',
+    Header: 'PO #',
+    Footer: 'PO #',
+    accessor: 'po_number',
     sticky: 'left'
   },
   {
-    Header: 'Date of Birth',
-    Footer: 'Date of Birth',
-    accessor: 'date_of_birth',
+    Header: 'Scheduled Ship Date',
+    Footer: 'Scheduled Ship Date',
+    accessor: 'scheduled_ship_date',
     Cell: ({ value }) => {
       return format(new Date(value), 'dd/MM/yyyy')
     }
   },
   {
-    Header: 'Country',
-    Footer: 'Country',
-    accessor: 'country'
+    Header: 'Ship To',
+    Footer: 'Ship To',
+    accessor: 'ship_to'
   },
   {
-    Header: 'Phone',
-    Footer: 'Phone',
-    accessor: 'phone'
-  },
-  {
-    Header: 'Email',
-    Footer: 'Email',
-    accessor: 'email'
-  },
-  {
-    Header: 'Age',
-    Footer: 'Age',
-    accessor: 'age'
+    Header: 'Amount',
+    Footer: 'Amount',
+    accessor: 'amount'
   },
 ]
 
-export const GROUPED_COLUMNS = [
-  {
-    Header: 'Id',
-    Footer: 'Id',
-    accessor: 'id'
-  },
-  {
-    Header: 'Name',
-    Footer: 'Name',
-    columns: [
-      {
-        Header: 'First Name',
-        Footer: 'First Name',
-        accessor: 'first_name'
-      },
-      {
-        Header: 'Last Name',
-        Footer: 'Last Name',
-        accessor: 'last_name'
-      }
-    ]
-  },
-  {
-    Header: 'Info',
-    Footer: 'Info',
-    columns: [
-      {
-        Header: 'Date of Birth',
-        Footer: 'Date of Birth',
-        accessor: 'date_of_birth'
-      },
-      {
-        Header: 'Country',
-        Footer: 'Country',
-        accessor: 'country'
-      },
-      {
-        Header: 'Phone',
-        Footer: 'Phone',
-        accessor: 'phone'
-      }
-    ]
-  }
-]
+// export const GROUPED_COLUMNS = [
+//   {
+//     Header: 'Id',
+//     Footer: 'Id',
+//     accessor: 'id'
+//   },
+//   {
+//     Header: 'Name',
+//     Footer: 'Name',
+//     columns: [
+//       {
+//         Header: 'First Name',
+//         Footer: 'First Name',
+//         accessor: 'first_name'
+//       },
+//       {
+//         Header: 'Last Name',
+//         Footer: 'Last Name',
+//         accessor: 'last_name'
+//       }
+//     ]
+//   },
+//   {
+//     Header: 'Info',
+//     Footer: 'Info',
+//     columns: [
+//       {
+//         Header: 'Date of Birth',
+//         Footer: 'Date of Birth',
+//         accessor: 'date_of_birth'
+//       },
+//       {
+//         Header: 'Country',
+//         Footer: 'Country',
+//         accessor: 'country'
+//       },
+//       {
+//         Header: 'Phone',
+//         Footer: 'Phone',
+//         accessor: 'phone'
+//       }
+//     ]
+//   }
+// ]
